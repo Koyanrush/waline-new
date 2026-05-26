e7e93c266c4cf479f3b13e008aef1086d1c93cb1
Koyanrush <1748341434@qq.com>
2026-05-27 01:53:04 +0800

    Update comment.js

--- api/comment.js
+++ api/comment.js
@@ -1,5 +1,17 @@
-const { createServer } = require('@waline/vercel');
+const { Server } = require('@waline/vercel');
 
-module.exports = createServer({
-  db: 'postgres'
+const waline = new Server({
+  db: 'postgres',
+  dbConfig: {
+    dialect: 'postgres',
+    host: 'aws-1-ap-southeast-1.pooler.supabase.com',
+    port: 5432, // 用回你最纯净的 5432 端口
+    username: 'postgres.hsuhbblpodwkxzxmqbbc',
+    password: 'pY@.$GwntmikTS9', // 在 V2 代码对象里，直接写原始密码，不需要转义
+    database: 'postgres',
+    ssl: { rejectUnauthorized: false },
+    schema: 'public'
+  }
 });
+
+module.exports = waline.netlify;
